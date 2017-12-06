8252688ba0fd90f0c77caea6e764d50e3935c32d
Roman Labunsky <roman@datree.io>
2017-12-06 14:57:41 +0200

    feat: added logger and basic logging when the correct env var exists

--- test/argumentsExtractor.test.js
+++ test/argumentsExtractor.test.js
@@ -4,30 +4,34 @@ require('babel-polyfill')
 const assert = require('assert')
 const args = require('../lib/argumentsExtractor')
 
-describe('Argv module', async function () {
+describe('Argv module', async function() {
   let packages, actionArgs
 
-  before(function () {
-    const argumentsExtractor = new args.ArgumentsExtractor(['sinon', 'co', '--save'])
+  before(function() {
+    const argumentsExtractor = new args.ArgumentsExtractor([
+      'sinon',
+      'co',
+      '--save'
+    ])
     packages = argumentsExtractor.extractPackagesArgv()
     actionArgs = argumentsExtractor.extractActionArgs()
   })
 
-  it('returns a package list when calling extractPackagesArgv', function () {
+  it('returns a package list when calling extractPackagesArgv', function() {
     assert.ok(packages.length === 2)
     assert.ok(packages.includes('sinon'))
     assert.ok(packages.includes('co'))
   })
 
-  it('does not include npm internal commands in package list when calling extractPackagesArgv', function () {
+  it('does not include npm internal commands in package list when calling extractPackagesArgv', function() {
     assert.ok(!packages.includes('--save'))
   })
 
-  it('returns action args when extractActionArgs is called', function () {
+  it('returns action args when extractActionArgs is called', function() {
     assert.deepEqual(actionArgs, ['--save'])
   })
 
-  it('excludes package names when extractActionArgs is called', function () {
+  it('excludes package names when extractActionArgs is called', function() {
     assert.ok(!actionArgs.includes('co'))
   })
 })
