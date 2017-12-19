3a6658ae867baa91a1647fdc600d2c19c9bd3af6
Roman Labunsky <roman@datree.io>
2017-12-19 12:13:18 +0200

    fix: removed fs-extra and refactored code to use fs instead

--- test/node.test.js
+++ test/node.test.js
@@ -1,9 +1,9 @@
 /* global describe it */
 
 require('babel-polyfill')
+const fs = require('fs')
 const node = require('../lib/node')
 const sinon = require('sinon')
-const fs = require('fs-extra')
 const assert = require('assert')
 
 let packageJsonStub = `{
@@ -49,11 +49,17 @@ let packageJsonStub = `{
       }
     }`
 
-describe('Node.js module', async function () {
-  describe('Get packages', async function () {
-    it('Should extract packages from package.json', async function () {
+describe('Node.js module', async function() {
+  describe('Get packages', async function() {
+    it('Should extract packages from package.json', async function() {
       try {
-        let deps = ['dependencies', 'devDependencies', 'bundledDependencies', 'peerDependencies', 'optionalDependencies']
+        let deps = [
+          'dependencies',
+          'devDependencies',
+          'bundledDependencies',
+          'peerDependencies',
+          'optionalDependencies'
+        ]
         let packages = await node.getPackages(JSON.parse(packageJsonStub))
 
         for (let dep of deps) {
@@ -65,8 +71,8 @@ describe('Node.js module', async function () {
     })
   })
 
-  it('Should check if packageJson exists', function () {
-    let stub = sinon.stub(fs, 'pathExistsSync').returns(false)
+  it('Should check if packageJson exists', function() {
+    let stub = sinon.stub(fs, 'existsSync').returns(false)
     let errmsg = 'should throw an error, package.json does not exist!'
 
     try {
@@ -83,9 +89,11 @@ describe('Node.js module', async function () {
     }
   })
 
-  it('Should read the package.json according to package and module name', function () {
+  it('Should read the package.json according to package and module name', function() {
     let packagename = node.NODEJS_PACKAGE_FILENAME
-    let stub = sinon.stub(fs, 'readJSONSync').returns({package: 'json'})
+    let stub = sinon
+      .stub(fs, 'readFileSync')
+      .returns(JSON.stringify({ package: 'json' }))
     let path = '/fake/path'
     try {
       node.readPackageJson(path)
@@ -97,9 +105,11 @@ describe('Node.js module', async function () {
     }
   })
 
-  describe('extractPackageInfo', async function () {
-    it('should return an empty object of package json is not found', async function () {
-      let stub = sinon.stub(node, 'packageJsonExists').throws('no package.json found')
+  describe('extractPackageInfo', async function() {
+    it('should return an empty object of package json is not found', async function() {
+      let stub = sinon
+        .stub(node, 'packageJsonExists')
+        .throws('no package.json found')
 
       try {
         let res = await node.extractPackageInfo('/fake/path')
@@ -111,10 +121,10 @@ describe('Node.js module', async function () {
       }
     })
 
-    it('Should extract data from package.json', async function () {
+    it('Should extract data from package.json', async function() {
       let packages = {
-        devDependencies:
-        { 'babel-cli': '^6.24.1',
+        devDependencies: {
+          'babel-cli': '^6.24.1',
           'babel-core': '^6.25.0',
           'babel-preset-env': '^1.6.0',
           'babel-polyfill': '^6.23.0',
@@ -122,22 +132,29 @@ describe('Node.js module', async function () {
           nyc: '^11.0.3',
           sinon: '^2.3.8',
           snazzy: '^7.0.0',
-          standard: '^10.0.2' },
-        dependencies:
-        { 'fs-extra': '^4.0.0',
+          standard: '^10.0.2'
+        },
+        dependencies: {
+          'fs-extra': '^4.0.0',
           request: '^2.81.0',
-          'request-promise-native': '^1.0.2' }
+          'request-promise-native': '^1.0.2'
+        }
       }
 
-      let stubPackageJsonExists = sinon.stub(fs, 'pathExistsSync').returns(true)
-      let stubReadPackageJson = sinon.stub(fs, 'readJSONSync').returns(JSON.parse(packageJsonStub))
+      let stubPackageJsonExists = sinon.stub(fs, 'existsSync').returns(true)
+      let stubReadPackageJson = sinon
+        .stub(fs, 'readFileSync')
+        .returns(packageJsonStub)
       let stubGetPackages = sinon.stub(node, 'getPackages').returns(packages)
 
       try {
         let res = await node.extractPackageInfo('/fake/path')
         assert.ok('packages' in res, 'packages not in package info')
         assert.ok('project_name' in res, 'project_name not in package info')
-        assert.ok('node_package_index' in res, 'node_package_index not in package info')
+        assert.ok(
+          'node_package_index' in res,
+          'node_package_index not in package info'
+        )
       } catch (err) {
         assert.ifError(err)
       } finally {
@@ -147,11 +164,13 @@ describe('Node.js module', async function () {
       }
     })
 
-    it('extracts repository name from a shorthand name', async function () {
-      const stubPackageJsonExists = sinon.stub(fs, 'pathExistsSync').returns(true)
+    it('extracts repository name from a shorthand name', async function() {
+      const stubPackageJsonExists = sinon.stub(fs, 'existsSync').returns(true)
       const alteredPackageJsonStub = JSON.parse(packageJsonStub)
       alteredPackageJsonStub.repository = 'bla:org/repo'
-      const stubReadPackageJson = sinon.stub(fs, 'readJSONSync').returns(alteredPackageJsonStub)
+      const stubReadPackageJson = sinon
+        .stub(fs, 'readFileSync')
+        .returns(JSON.stringify(alteredPackageJsonStub))
       const stubGetPackages = sinon.stub(node, 'getPackages').returns({})
       try {
         const res = await node.extractPackageInfo('/fake/path')
@@ -165,11 +184,13 @@ describe('Node.js module', async function () {
       }
     })
 
-    it('extracts repository name from a shorthand name with a branch or tag (#)', async function () {
-      const stubPackageJsonExists = sinon.stub(fs, 'pathExistsSync').returns(true)
+    it('extracts repository name from a shorthand name with a branch or tag (#)', async function() {
+      const stubPackageJsonExists = sinon.stub(fs, 'existsSync').returns(true)
       const alteredPackageJsonStub = JSON.parse(packageJsonStub)
       alteredPackageJsonStub.repository = 'bla:org/repo#branch'
-      const stubReadPackageJson = sinon.stub(fs, 'readJSONSync').returns(alteredPackageJsonStub)
+      const stubReadPackageJson = sinon
+        .stub(fs, 'readFileSync')
+        .returns(JSON.stringify(alteredPackageJsonStub))
       const stubGetPackages = sinon.stub(node, 'getPackages').returns({})
       try {
         const res = await node.extractPackageInfo('/fake/path')
@@ -183,11 +204,13 @@ describe('Node.js module', async function () {
       }
     })
 
-    it('extracts repository name from a shorthand simple name', function () {
-      const stubPackageJsonExists = sinon.stub(fs, 'pathExistsSync').returns(true)
+    it('extracts repository name from a shorthand simple name', function() {
+      const stubPackageJsonExists = sinon.stub(fs, 'existsSync').returns(true)
       const alteredPackageJsonStub = JSON.parse(packageJsonStub)
       alteredPackageJsonStub.repository = 'repo'
-      const stubReadPackageJson = sinon.stub(fs, 'readJSONSync').returns(alteredPackageJsonStub)
+      const stubReadPackageJson = sinon
+        .stub(fs, 'readFileSync')
+        .returns(JSON.stringify(alteredPackageJsonStub))
       const stubGetPackages = sinon.stub(node, 'getPackages').returns({})
       try {
         const res = node.extractPackageInfo('/fake/path')
@@ -201,14 +224,16 @@ describe('Node.js module', async function () {
       }
     })
 
-    it('extracts repository name from git url', function () {
-      const stubPackageJsonExists = sinon.stub(fs, 'pathExistsSync').returns(true)
+    it('extracts repository name from git url', function() {
+      const stubPackageJsonExists = sinon.stub(fs, 'existsSync').returns(true)
       const alteredPackageJsonStub = JSON.parse(packageJsonStub)
       alteredPackageJsonStub.repository = {
         type: 'git',
         url: 'https://github.com/npm/repo.git'
       }
-      const stubReadPackageJson = sinon.stub(fs, 'readJSONSync').returns(alteredPackageJsonStub)
+      const stubReadPackageJson = sinon
+        .stub(fs, 'readFileSync')
+        .returns(JSON.stringify(alteredPackageJsonStub))
       const stubGetPackages = sinon.stub(node, 'getPackages').returns({})
       try {
         const res = node.extractPackageInfo('/fake/path')
@@ -222,14 +247,16 @@ describe('Node.js module', async function () {
       }
     })
 
-    it('extracts repository name from svn url', function () {
-      const stubPackageJsonExists = sinon.stub(fs, 'pathExistsSync').returns(true)
+    it('extracts repository name from svn url', function() {
+      const stubPackageJsonExists = sinon.stub(fs, 'existsSync').returns(true)
       const alteredPackageJsonStub = JSON.parse(packageJsonStub)
       alteredPackageJsonStub.repository = {
         type: 'svn',
         url: 'https://svn.com/repo'
       }
-      const stubReadPackageJson = sinon.stub(fs, 'readJSONSync').returns(alteredPackageJsonStub)
+      const stubReadPackageJson = sinon
+        .stub(fs, 'readFileSync')
+        .returns(JSON.stringify(alteredPackageJsonStub))
       const stubGetPackages = sinon.stub(node, 'getPackages').returns({})
       try {
         const res = node.extractPackageInfo('/fake/path')
@@ -243,14 +270,16 @@ describe('Node.js module', async function () {
       }
     })
 
-    it('returns an empty string for an unknown url type', function () {
-      const stubPackageJsonExists = sinon.stub(fs, 'pathExistsSync').returns(true)
+    it('returns an empty string for an unknown url type', function() {
+      const stubPackageJsonExists = sinon.stub(fs, 'existsSync').returns(true)
       const alteredPackageJsonStub = JSON.parse(packageJsonStub)
       alteredPackageJsonStub.repository = {
         type: 'mercurial',
         url: 'https://svn.com/repo'
       }
-      const stubReadPackageJson = sinon.stub(fs, 'readJSONSync').returns(alteredPackageJsonStub)
+      const stubReadPackageJson = sinon
+        .stub(fs, 'readFileSync')
+        .returns(JSON.stringify(alteredPackageJsonStub))
       const stubGetPackages = sinon.stub(node, 'getPackages').returns({})
       try {
         const res = node.extractPackageInfo('/fake/path')
